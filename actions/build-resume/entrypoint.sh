421056cb7a557d0d6cf88fbed61992e40e2d841e
Nicola Lamacchia <nicola.lamacchia@gmail.com>
2021-03-07 21:57:14 +0100

    Add workflow and action to build the resume

---
+++ actions/build-resume/entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+echo "Creating the output directory $OUT_DIR..."
+mkdir -p "$OUT_DIR"
+
+for filename in *.tex *.cls; do
+    [ -f "$filename" ] || break
+    echo "Updating the build number ${GITHUB_SHA::7} in file $filename..."
+    sed -ie "s/%BuildHash%/${GITHUB_SHA::7}/" "$filename"
+done
+
+for filename in *.tex; do
+    [ -f "$filename" ] || break
+    echo "Converting $filename to PDF..."
+    xelatex "$filename"
+done
+
+cp "*.pdf" "$OUT_DIR"
